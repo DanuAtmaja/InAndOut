0fa667afcdc45adbb5796371f43e10a7df10f4e1
Danu Atmaja <danuatmaja@gmail.com>
2021-06-26 15:01:00 +0800

    Generated Create Item Feature

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace InAndOut.Models
@@ -9,6 +10,7 @@ namespace InAndOut.Models
         public int Id { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
+        [DisplayName("Item Name")]
         public string ItemName { get; set; }
     }
 }
