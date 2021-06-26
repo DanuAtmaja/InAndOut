888b0472231fc8b9d8d2bcb7143adb7d873f70f1
Danu Atmaja <danuatmaja@gmail.com>
2021-06-26 16:23:41 +0800

    Generated Expense Controller for Create and View, added Migration for expense and developed server side validation for create expense

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
