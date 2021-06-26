f5465291669e7ea6eb2904a8f4a3262bc4e62299
Danu Atmaja <danuatmaja@gmail.com>
2021-06-26 11:28:47 +0800

    Initial check-in of module InAndOut

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: /appointment/
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+
+        // GET: /appointment/details/
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = {id}" + id);
+        }
+    }
+}
